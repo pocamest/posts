720513f00de5ae104e436cf3855eb266152d4603
pocamest <pocamest@yandex.ru>
2025-03-29 21:28:58 +0300

    Fix installed_apps in settings

--- posts/settings.py
+++ posts/settings.py
@@ -14,14 +14,13 @@ import os
 from pathlib import Path
 from environ import Env
 
-
-env = Env()
-env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+env = Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -46,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
 
     'post_api'
 ]
