4abcf58f0bf65e543fc8da88c96041800a5788c2
pocamest <pocamest@yandex.ru>
2025-03-29 14:39:14 +0300

    Add model Post

--- post_api/models.py
+++ post_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name='posts'
+    )
+    text = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author.username}: {self.text[:30]}...'
