4fc01b9d147e6d768a3f56d7f0cd25b367b2618a
pocamest <pocamest@yandex.ru>
2025-04-27 00:10:58 +0300

    Add get_queryset() and perform_create()

--- post_api/views.py
+++ post_api/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from post_api import serializers, models
+from django.db.models import Count
 
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.PostSerializer
-    queryset = models.Post.objects.all()
+
+    def get_queryset(self):
+        return models.Post.objects.annotate(
+            likes_count=Count('post_likes')
+        ).select_related('author').prefetch_related('images', 'post_comments')
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
