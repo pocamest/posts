fe8af2b41a91ad76da6a2831fb808aaf911d4048
pocamest <pocamest@yandex.ru>
2025-04-27 01:10:51 +0300

    Add permission_classes in PostViewSet

--- post_api/views.py
+++ post_api/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from post_api import serializers, models
+from .permissions import IsAuthorOrReadOnly
 from django.db.models import Count
 
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.PostSerializer
+    permission_classes = [IsAuthorOrReadOnly]
 
     def get_queryset(self):
         return models.Post.objects.annotate(
