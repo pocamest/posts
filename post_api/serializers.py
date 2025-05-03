9610ee64b72b37a376fd81fec41d0d56292b35d1
pocamest <pocamest@yandex.ru>
2025-05-03 22:52:19 +0300

    Add read_only_fields in CommentSerializer

--- post_api/serializers.py
+++ post_api/serializers.py
@@ -16,6 +16,7 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Comment
         fields = ['id', 'author', 'text', 'created_at']
+        read_only_fields = ['author', 'created_at']
 
 
 class PostSerializer(serializers.ModelSerializer):
